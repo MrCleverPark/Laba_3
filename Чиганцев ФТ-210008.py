16c898b9cebff539e1f7298c739f1063ff1429d5
MrCleverPark <andrew.chigantcev@yandex.ru>
2022-09-24 20:45:34 +0500

    Update Чиганцев ФТ-210008.py

--- Чиганцев ФТ-210008.py
+++ Чиганцев ФТ-210008.py
@@ -5,3 +5,19 @@ lang=input('Введите язык RU или EU: ')
 number=int(input('Введите число на которое будет сдвигаться буква: '))
 d = ''
 if lang == 'RU':
+    for i in message:
+        place=ru.find(i) 
+        new=place+number
+        if i in ru:
+            d+=ru[new]
+        else:
+            d+=i
+else:
+    for i in message:
+        place=eu.find(i) 
+        new=place+number
+        if i in eu:
+            d+=eu[new]
+        else:
+            d+=i
+print(d)
