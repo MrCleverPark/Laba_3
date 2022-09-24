0d79f5881c4e9f997fab8d895fa1d5d217f76a73
MrCleverPark <andrew.chigantcev@yandex.ru>
2022-09-24 20:43:11 +0500

    Create Чиганцев ФТ-210008.py
    Создали алфавит

---
+++ Чиганцев ФТ-210008.py
@@ -0,0 +1,7 @@
+ru = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЭЮЯ'
+eu = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
+message=input('Введите текст заглавными буквами Русского или Английского алфавита: ')
+lang=input('Введите язык RU или EU: ')
+number=int(input('Введите число на которое будет сдвигаться буква: '))
+d = ''
+if lang == 'RU':
