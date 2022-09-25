4b500c65e4890bb75a8e642645469f6a21904010
MrCleverPark <andrew.chigantcev@yandex.ru>
2022-09-25 14:44:52 +0500

    Update Чиганцев ФТ-210008.py
    теперь код может работать даже с большим шагом

--- Чиганцев ФТ-210008.py
+++ Чиганцев ФТ-210008.py
@@ -1,8 +1,8 @@
-ru = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЭЮЯ'
-eu = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
-message=input('Введите текст заглавными буквами Русского или Английского алфавита: ')
+ru = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЭЮЯ'*1000000
+eu = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'*10000000
+message=input('Введите текст ЗАГЛАВНЫМИ буквами Русского или Английского алфавита: ')
 lang=input('Введите язык RU или EU: ')
-number=int(input('Введите число на которое будет сдвигаться буква: '))
+number=int(input('Введите шаг сдвига '))
 d = ''
 if lang == 'RU':
     for i in message:
